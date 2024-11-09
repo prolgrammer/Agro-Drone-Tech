b0ff02db41e46a5271bd897dc2e53068d1f88058
ib1te1 <timalek123@mail.ru>
2024-11-10 03:22:15 +0400

    from zoo to kraft

--- ADT/producer/producer.py
+++ ADT/producer/producer.py
@@ -5,37 +5,35 @@ import logging
 import base64
 import json
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-# Логика повторного подключения
+logging.info("Запуск producer.py начат")
+
 producer = None
 while not producer:
     try:
+        logging.info("Попытка подключения к Kafka...")
         producer = KafkaProducer(
-            bootstrap_servers=['kafka:9092'],
-            value_serializer=lambda v: json.dumps(v).encode('utf-8')  # Кодирование JSON
+            bootstrap_servers=[os.getenv("KAFKA_BROKER", "kafka:9092")],
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
         )
         logging.info("Успешное подключение к Kafka")
     except Exception as e:
         logging.error(f"Kafka недоступен, повторная попытка... Ошибка: {e}")
         time.sleep(5)
 
-# Папка с изображениями для отправки
 image_folder = 'images_to_send'
-
-# Пример параметров
 user_id = "user_123"
 field_id = "field_456"
 coordinates = {"latitude": 35.6895, "longitude": 139.6917}
 soil_type = "Песчаная"
 crop_type = "wheat"
 
-# Отправляем изображения в Kafka
+logging.info("Начинаем отправку изображений")
+
 for filename in os.listdir(image_folder):
     if filename.endswith(".jpg") or filename.endswith(".png"):
         with open(os.path.join(image_folder, filename), 'rb') as image_file:
-            # Кодируем изображение в base64
             image_data_base64 = base64.b64encode(image_file.read()).decode('utf-8')
             message = {
                 "user_id": user_id,
@@ -46,7 +44,8 @@ for filename in os.listdir(image_folder):
                 "image_data": image_data_base64
             }
             producer.send('image_topic', message)
-            logging.info(f'Изображение {filename} и параметры отправлены в Kafka')
-        time.sleep(2)  # Пауза, чтобы консюмер успел обработать данные
+            logging.info(f'Изображение {filename} отправлено в Kafka')
+        time.sleep(2)
 
+logging.info("Завершение работы producer.py")
 producer.close()
